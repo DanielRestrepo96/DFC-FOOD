ce5fa4b80a675050989265fcb3dea41661c29ee2
daniel96 <darema2012@gmail.com>
2020-08-29 19:01:22 -0500

    primera entrega

--- index.php
+++ index.php
@@ -1,10 +1,26 @@
 .<!DOCTYPE html>
 <html>
 <head>
-	<title>daniel</title>
+	<title>COMIDAS RÁPIDAS DFC</title>
 </head>
-<body>
+<body> 
+	<center> <img src="img/logo empresa.jpg" width="80%"> 
 	<?php ?>
- lenguaje para la web
+	<h1>DESCRIPCIÓN</h1> 
+	<P>Esta página será empleada para comidas rápidas, <br>
+	al final del semestre deseamos ver que esta página sea funcional para una empresa, <br> deseamos que el usuario final observe todos aquellos productos que la empresa publique en dicha página, <br> así como tambièn que se haga contacto con el dueño de la empresa o bien el que esté a cargo de  las ventas y domicilios</P> 
+	<h1>MISIÓN</h1> 
+<p>Nuestra misión es satisfacer las necesidades del sector alimentario,<br> mediante la elaboración y comercialización de productos y servicios de óptima calidad, <br> que se ajusten a las necesidades de nuestros clientes, al generar economía, <br> desarrollo y crecimiento para el sector productos, para empleados y accionistas.</p>
+
+<h1>VISIÓN</h1>
+<p> Para el año 2022 dominar la industria global de servicios alimenticios, a través de la satisfacción del cliente. </p>
+<h1> VALORES </h1>
+<p><h3>COMPROMISO:</h3> Cumplimos con lo que prometemos y cada DÍA somos mejores.
+ <br><h3>HONESTIDAD:</h3> Somos coherentes entre lo que pensamos, decimos y actuamos, asumimos nuestros actos.
+<br><h3>PERSEVERANCIA:</h3> Somos persistentes, actuamos con decisión y constancia.</p>
+<a href= "https://www.elespanol.com/cocinillas/recetas/20151001/cenas-rapidas-no-complicarse-vida-mes/1000273022696_30.html"> <h2>Cocinillas</h2> </a> 
+<h3>RECETAS</h3> 
+<iframe width="560" height="315" src="https://www.youtube.com/embed/9z_Zs05IFlg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+ <a href="contactenos.html" target="_blank"> <h2> CONTACTENOS </h2></a> </center>
 </body>
 </html>
