feada026473520c1a6274e78030631c39094b789
fernanda81 <mariafernandatoro81>
2020-09-27 13:25:40 -0500

    segunda entrega

--- index.php
+++ index.php
@@ -1,26 +1,282 @@
 .<!DOCTYPE html>
 <html>
-<head>
-	<title>COMIDAS RÁPIDAS DFC</title>
-</head>
-<body> 
-	<center> <img src="img/logo empresa.jpg" width="80%"> 
-		<?php ?>
-		<h1>DESCRIPCIÓN</h1> 
-		<P>Esta página será empleada para comidas rápidas, <br>
-			al final del semestre deseamos ver que esta página sea funcional para una empresa, <br> deseamos que el usuario final observe todos aquellos productos que la empresa publique en dicha página, <br> así como tambièn que se haga contacto con el dueño de la empresa o bien el que esté a cargo de  las ventas y domicilios</P> 
-			<h1>MISIÓN</h1> 
-			<p>Nuestra misión es satisfacer las necesidades del sector alimentario,<br> mediante la elaboración y comercialización de productos y servicios de óptima calidad, <br> que se ajusten a las necesidades de nuestros clientes, al generar economía, <br> desarrollo y crecimiento para el sector productos, para empleados y accionistas.</p>
+    <head>
+	    <meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	    <title>COMIDAS RÁPIDAS DFC</title>   
+	    <link rel="stylesheet" type="text/css" href="estilos/index.css ">
+	   
 
-			<h1>VISIÓN</h1>
-			<p> Para el año 2022 dominar la industria global de servicios alimenticios, a través de la satisfacción del cliente. </p>
-			<h1> VALORES </h1>
-			<p><h3>COMPROMISO:</h3> Cumplimos con lo que prometemos y cada DÍA somos mejores.
-				<br><h3>HONESTIDAD:</h3> Somos coherentes entre lo que pensamos, decimos y actuamos, asumimos nuestros actos.
-				<br><h3>PERSEVERANCIA:</h3> Somos persistentes, actuamos con decisión y constancia.</p>
-				<a href= "https://www.elespanol.com/cocinillas/recetas/20151001/cenas-rapidas-no-complicarse-vida-mes/1000273022696_30.html"> <h2>Cocinillas</h2> </a> 
-				<h3>RECETAS</h3> 
-				<iframe width="560" height="315" src="https://www.youtube.com/embed/9z_Zs05IFlg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-				<a href="contactenos.html" target="_blank"> <h2> CONTACTENOS </h2></a> </center>
-			</body>
-			</html>
+    </head>
+  <body>
+	  <?php ?>	   
+	   
+	   
+	<header>
+		<div class="buscar">
+		   <input class="btext" type="text" placeholder="Escribe lo que buscas">
+		    <a  href=""  class="boton1">
+			<i class="icon-buscar"></i>
+		   </a>
+	   </div>
+		    <center>
+		      <img src="img/logo empresa.png" width="80%">
+		    </center>				
+	</header>
+	<nav>
+		  <ul class=" navi" >
+			 <li class="list"><a class="etiq" href="index.html">Home</a></li>
+			 <li class="list"><a class="etiq" href="quien somos.html">Quien somos</a></li>
+			 <li class="list"><a class="etiq" target="_blank" href="ubicanos.html">Nuestras sedes</a></li>			
+			 <li class="list"><a class="etiq" href="dfc kids.html"> DFC Kids</a></li>
+			 <li class="list"><a class="etiq" href="contactenos.html">Contactanos</a></li>
+			 <li class="list"><a class="etiq" target="_blank" href="login.html">Log in</a></li>	
+		  </ul>			           
+	   </nav>	   	
+	  
+		<section>
+			<h2> EMPANADAS</h2>
+			<center>
+			 <img src="img/empanada.jpg" width="65%">
+			 </center>
+			 <article id="uno">
+			 	<aside>
+			 		<h4> Gaseosas</h4>
+			 		<ul>
+						<li>Coca cola </li>
+						<li>Pepsi </li>
+						<li>7 up </li>						
+					</ul> 
+					<ul>
+						<li>Cuatro </li>
+						<li>Uva </li>
+						<li>Colombiana</li>
+					</ul>	 
+			 	</aside>
+			 	<aside>
+			 		<h4>Precio</h4>
+					<p> 15 X $ 5000 </p>
+					<h4>Precio especial</h4>
+					<p>20 X $ 10.000 más gaseosa</p>
+			 	</aside>
+			 </article>
+		</section>
+		<section>
+		  	<h2> SALCHIPAPAS</h2>
+		  		<center>		  	
+		  		<img src="img/salchipapa_nota.jpg" width="50%">	
+			  	</center>
+			<article id="dos">
+				
+				<aside>
+					<h4>Gaseosas</h4>
+			 		<ul>						
+						<li>Coca cola</li>
+						<li>Pepsi</li>
+						<li>7 up</li>
+					</ul> 
+					<ul>						
+						<li>Cuatro</li>
+						<li>Uva</li>
+						<li>Colombiana</li>
+					</ul> 
+
+			 	</aside>
+			 	<aside>
+			 		<h4>Precio</h4>
+					<p>$ 7.000 </p>					
+					<h4>Precio especial</h4>
+					<p> $ 11.000 más gaseosa </p>
+			 	</aside>
+			</article> 
+		  </section>
+		   
+		 </section>
+		<section>
+			<h2> PERROS ESPECIALES</h2>
+			<center>
+			 <img src="img/hot dog especial.jpg" width="55%">
+			 </center>
+			 <article id="tres">
+			 	<aside>
+			 		<h4>Gaseosas</h4>
+			 		<ul>
+						<li>Coca cola</li>
+						<li>Pepsi</li>
+						<li>7 up</li>
+					</ul> 
+					<ul>
+						<li>Cuatro</li>
+						<li>Uva</li>
+						<li>Colombiana</li>
+					</ul> 	 	 
+			 	</aside>
+			 	<aside>
+			 		<h4>Precio</h4>
+					<p> $ 7.000 </p>
+					<h4>Precio especial</h4>
+					<p> $ 13.000 más limonada </p>
+			 	</aside>
+			 </article>
+		  </section>
+		  <section>
+		  	<h2> HAMBURGUESAS SENCILLAS</h2>
+		  	<center>
+				<img src="img/burger sencilla.jpeg" width="55%">
+			</center>
+			<article id="cuatro">
+				<aside>
+			 		<h4>Gaseosas</h4>
+			 		<ul>
+						<li>Coca cola</li>
+						<li>Pepsi</li>
+						<li>7 up</li>
+					</ul> 	
+					<ul>
+						<li>Cuatro</li>
+						<li>Uva</li>
+						<li>Colombiana</li>
+					</ul>  
+			 	</aside>
+			 	<aside>
+			 		<h4>Precio</h4>
+					<p> $ 8.000 </p>					
+					<h4>Precio especial</h4>
+					<p> $ 10.000 más gaseosa </p>
+			 	</aside>
+			</article> 
+		  </section>
+		   
+		 </section>
+		<section>
+			<h2> PERROS SENCILLOS</h2>
+			<center>
+			 <img src="img/hot dog sencillo.jpg" width="65%">
+			 </center>
+			 <article id="cinco">
+			 	<aside>
+			 		<h4>Gaseosas</h4>
+			 		<ul>
+						<li>Coca cola</li>
+						<li>Pepsi</li>
+						<li>7 up</li>						
+					</ul> 	 
+					<ul>
+						<li>Cuatro</li>
+						<li>Uva</li>
+						<li>Colombiana</li>							
+					</ul> 	 
+			 	</aside>
+			 	<aside>
+			 		<h4>Precio</h4>
+						<p>$ 6.500 </p>					
+						<h4>Precio especial</h4>
+						<p> $ 7.500 más gaseosa </p>
+			 	</aside>
+			 </article>
+		  </section>
+		  <section>
+		  	<h2> HAMBURGUESAS ESPECIALES</h2>
+		  	<center>
+		  	   <img src="img/burger especial.jpg" width="58%">
+		  	</center>
+			<article id="seis">
+				<aside>
+			 		<h4>Gaseosas</h4>
+			 		<ul>						
+						<li>Coca cola</li>
+						<li>Pepsi</li>
+						<li>7 up</li>
+					</ul> 	
+					<ul>						
+						<li>Cuatro</li>
+						<li>Uva</li>
+						<li>Colombiana</li>
+					</ul>  
+			 	</aside>
+			 	<aside>
+			 		<h4>Precio</h4>
+					<p>$ 9000 </p>					
+					<h4>Precio especial</h4>
+					<p> $ 12.500 más gaseosa </p>
+			 	</aside>
+			</article> 
+		  </section>
+		   
+		 </section>
+		<section>
+			<h2> ALITAS BBQ</h2>
+			<center>			 
+				<img src="img/alitas bbq.jpg" width="54%">
+			 </center>
+			 <article id="siete">
+			 	<aside>
+			 		<h4>Gaseosas</h4>
+			 		<ul>						
+						<li>Coca cola</li>
+						<li>Pepsi</li>
+						<li>7 up</li>
+					</ul> 
+					<ul>						
+						<li>Cuatro</li>
+						<li>Uva</li>
+						<li>Colombiana</li>
+					</ul> 	 
+			 	</aside>
+			 	<aside>
+			 		<h4>Precio</h4>
+					<p> $ 12.000 </p>					
+					<h4>Precio especial</h4>
+					<p> $ 20.000 más gaseosa </p>
+			 	</aside>
+			 </article>
+		  </section>
+		  <section>
+		  	<h2> MANDIGAS</h2>
+		  	<center>			 
+				<img src="img/mandinga.jpg" width="44%" >
+			 </center>
+			<article id="ocho">
+				<aside>
+			 		<h4>Gaseosas</h4>
+			 		<ul>						
+						<li>Coca cola</li>
+						<li>Pepsi</li>
+						<li>7 up</li>
+					</ul> 
+					<ul>						
+						<li>Cuatro</li>
+						<li>Uva</li>
+						<li>Colombiana</li>
+					</ul> 
+			 	</aside>
+			 	<aside>
+			 		<h4>Precio</h4>
+					<p> $ 15.000 </p>					
+					<h4>Precio especial</h4>
+					<p> $ 20.000 más gaseosa </p>
+			 	</aside>
+			</article> 
+		  </section>
+		 </section>
+		
+		<a href= "https://www.elespanol.com/cocinillas/recetas/20151001/cenas-rapidas-no-complicarse-vida-mes/1000273022696_30.html"> <h2>Cocinillas</h2> </a>
+					 
+	    <h3>RECETAS</h3> 
+		<iframe  src="https://www.youtube.com/embed/9z_Zs05IFlg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			
+				<hr style="color: darkgray">
+		<footer class="pie">
+			<ul>
+              <li>CORREO: DFCFOOD@GMAIL.COM</li>
+              <li>TELEFONO: 5230990</li>
+              <li>DIRECCIÓN: CR 50 # 104- 60</li>
+            </ul>
+            <ul>
+              <li>Facebook</li>
+              <li>Youtibe</li>
+              <li>Instagram</li>
+            </ul>
+		</footer>
+	</body>
+</html>
